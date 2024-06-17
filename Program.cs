1b6aeb51e21b9124e1ac5be646b2e44635350dda
tdownie0 <tylerdownie0@gmail.com>
2024-06-17 18:12:26 -0400

    Add backend logic.

--- Program.cs
+++ Program.cs
@@ -1,6 +1,37 @@
+using Microsoft.AspNetCore.Http.HttpResults;
+using Microsoft.AspNetCore.Rewrite;
+
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+app.UseRewriter(new RewriteOptions().AddRedirect("tasks/(.*)", "todos/$1"));
+app.Use(async (context, next) => 
+{
+    Console.WriteLine($"[{context.Request.Method} {context.Request.Path} {DateTime.UtcNow}] Started.");
+    await next(context);
+    Console.WriteLine($"[{context.Request.Method} {context.Request.Path} {DateTime.UtcNow}] Finished.");
+});
+
+var todos = new List<Todo>();
+app.MapGet("/todos", () => todos);
+app.MapGet("/todos/{id}", Results<Ok<Todo>, NotFound> (int id) =>
+{
+    var targetTodo = todos.SingleOrDefault(t => id == t.Id);
+    return targetTodo is null
+        ? TypedResults.NotFound()
+        : TypedResults.Ok(targetTodo);
+});
+app.MapPost("/todos", (Todo task) => 
+{
+    todos.Add(task);
+    return TypedResults.Created("/todos/{id}", task);
+});
+app.MapDelete("/todos/{id}", (int id) => 
+{
+    todos.RemoveAll(t => id == t.Id);
+    return TypedResults.NoContent();
+});
 
 app.Run();
+
+public record Todo(int Id, string Name, DateTime DueDate, bool IsCompleted);
